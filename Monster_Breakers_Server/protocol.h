7b67614d3a8da239146240d2b979065c12de3d9c
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2025-10-22 06:23:50 +0900

    기초 다지는중

---
+++ Monster_Breakers_Server/protocol.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "common.h"
+
+//#define SET_DATA_FROM_DATABASE
+#define SERVER_STRESS_TEST
+
+#define MAX_PACKET_SIZE 1024
+#define SERVER_PORT 3000
+#define NUM_WORKER_THREADS 4
+#define MAX_USER 5000
+
+#define MAX_BUFFER 8192
+
+constexpr char MAX_ID_LENGTH = 20;
+
+// sc 관련
+constexpr char SC_P_USER_INFO = 1;
+constexpr char SC_P_MOVE = 2;
+constexpr char SC_P_ENTER = 3;
+constexpr char SC_P_LEAVE = 4;
+
+// cs 관련
+constexpr char CS_P_LOGIN = 5;
+constexpr char CS_P_MOVE = 6;
+
+
+
+struct sc_packet_user_info {
+	unsigned char	size;
+	char			type;
+	long long		id;
+	XMFLOAT3		position;
+	XMFLOAT3		look;
+	XMFLOAT3		right;
+	uint8_t			animState;
+	short			hp;
+};
+
+
+struct sc_packet_move {
+	unsigned char		size;
+	char				type;
+	long long			id;
+	XMFLOAT3			position;
+	XMFLOAT3			look;
+	XMFLOAT3			right;
+	uint8_t				animState;
+};
+
+
+struct sc_packet_enter {
+	unsigned char		size;
+	char				type;
+	long long			id;
+	XMFLOAT3			position;
+	XMFLOAT3			look;
+	XMFLOAT3			right;
+	uint8_t				animState;
+	short				hp;
+};
+
+struct sc_packet_leave {
+	unsigned char		size;
+	char				type;
+	long long			id;
+};
+
+
+struct cs_packet_login {
+	unsigned char		size;
+	char				type;
+	//XMFLOAT3			position;
+	char				name[MAX_ID_LENGTH];
+
+};
+
+
+struct sc_packet_login_fail {
+	unsigned char		size;
+	char				type;
+};
+
+struct cs_packet_move {
+	unsigned char		size;
+	char				type;
+	XMFLOAT3			position;
+	XMFLOAT3			look;
+	XMFLOAT3			right;
+	uint8_t				animState;
+};
--- Monster_Breakers_Server/workerthread.h
+++ Monster_Breakers_Server/workerthread.h
@@ -1,2 +1,55 @@
 #pragma once
 #include "common.h"
+
+class SESSION;
+
+// 전역 변수 선언
+extern HANDLE g_hIOCP;
+extern std::atomic<long long> g_client_counter;
+extern std::unordered_map<long long, SESSION*> g_user;
+extern std::mutex g_session_mutex;
+extern SOCKET g_listen_socket;
+
+enum IO_OP { IO_RECV, IO_SEND, IO_ACCEPT };
+
+
+class EXP_OVER
+{
+public:
+	EXP_OVER(IO_OP op);
+
+
+	WSAOVERLAPPED		_over;
+	IO_OP				_io_op;
+	SOCKET				_accept_socket;
+	unsigned char		_buffer[1024];
+	WSABUF				_wsabuf[1];
+
+
+};
+
+
+class SESSION {
+public:
+	SOCKET				_c_socket;
+	long long			_id;
+
+	EXP_OVER			_recv_over{ IO_RECV };
+	unsigned char		_remained;
+
+	XMFLOAT3			_position;
+	XMFLOAT3			_look;
+	XMFLOAT3			_right;
+	uint8_t				_animState;
+	short				_hp = 100;
+	string				_name;
+	atomic<bool>		_is_sending{ false };
+
+public:
+	SESSION() = delete;
+
+
+	SESSION(long long session_id, SOCKET s);
+
+
+};
